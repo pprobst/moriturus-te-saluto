bcd9f29c4beb548cf319ed24f63757ff06e14099
pprobst <pprobst@insiberia.net>
2018-01-15 13:27:35 -0200

    vitae19

---
+++ ch_1/ex1-19.scm
@@ -0,0 +1,26 @@
+; Exercise 1.19:
+
+; I wasn't able to figure this out by myself.
+
+; See :
+; http://www.billthelizard.com/2010/01/sicp-exercise-119-computing-fibonacci.html
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p ) (* q q))
+                   (+ (* 2 p q) (* q q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(fib 5)
+(fib 20)
