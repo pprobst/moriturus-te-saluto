a944c806b5e652fcbe482f373e1d3add3850cfb0
pprobst <pprobst@insiberia.net>
2018-01-24 14:19:11 -0200

    INCOMPLETE

---
+++ ch_1/ex1-33.scm
@@ -0,0 +1,13 @@
+; Exercise 1.33:
+; Write an even more general version of accumulate called filtered-accumulate.
+; Show how to express:
+; a) the sum of the squares of the prime numbers between a and b (assuming you
+;    have a prime? predicate already written.
+; b) the product of all the positive integers less than n that are relatively
+;    prime to n (i.e., all positive integers i < n such that GCD(i, n) = 1).
+
+; filtered-accumulate procedure
+
+; Auxiliary procedures
+
+; Tests
