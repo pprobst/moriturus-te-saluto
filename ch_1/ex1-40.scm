0995fd7a3c6b7f3dcb44cdf91dced9e355f522e6
pprobst <pprobst@insiberia.net>
2018-01-30 16:36:54 -0200

    vitae40

---
+++ ch_1/ex1-40.scm
@@ -0,0 +1,46 @@
+; Exercise 1.40:
+; Define a procedure cubic that can be used together with 
+; the newtons-method procedure in expressions of the form
+; (newtons-method (cubic a b c) 1)
+; to approximate zeroes of the cubic x³ + ax² + bx + c 
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x)
+       (* a (square x))
+       (* b x)
+       c)))
+
+(newtons-method (cubic 4 3 12) 1)
