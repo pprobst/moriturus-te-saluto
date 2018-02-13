18abbaf27d2e093733166a901d00a12b42f41109
pprobst <pprobst@insiberia.net>
2018-02-13 17:50:21 -0200

    6

--- ch_2/ex2-06.scm
+++ ch_2/ex2-06.scm
@@ -25,4 +25,5 @@
 (define one (lambda (f) (lambda (x) (f x))))
 (define two (lambda (f) (lambda (x) (f (f x)))))
 
-(define add ()) ; to be completed
+(define (add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
