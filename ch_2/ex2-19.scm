ee1bec7259508dd5a7362406dfc9a13e0b9e4f32
pprobst <pprobst@insiberia.net>
2018-02-21 15:28:30 -0300

    19

---
+++ ch_2/ex2-19.scm
@@ -0,0 +1,36 @@
+; Exercise 2.19:
+; Define the procedures first-denomination, except-first-denomination, and
+; no-more? in terms of primitive operations on list structures. Does the 
+; order of the list coin-values affect the answer produced by cc? Why or
+; why not?
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination
+                   coin-values))
+             (cc (- amount
+                    (first-denomination
+                      coin-values))
+                 coin-values)))))
+
+(define (no-more? coins)
+  (null? coins))
+
+(define (first-denomination coins)
+  (car coins))
+
+(define (except-first-denomination coins)
+  (cdr coins))
+
+(cc 100 us-coins)
+(cc 100 uk-coins)
+
+; The order doesn't matter, because the algorithm implemented recusively 
+; evaluates all the combinations possible regardless of the order.
+
