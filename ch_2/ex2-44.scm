1d5866a0a333292c3c28b545681e719b584e9f5b
pprobst <pprobst@insiberia.net>
2018-04-02 21:45:21 -0300

    44

---
+++ ch_2/ex2-44.scm
@@ -0,0 +1,17 @@
+; Exercise 2.44:
+; Define the procedure up-split used by corner-split. It is similar to
+; right-split, except that it switches the roles of below and beside.
+
+; Right-split
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+; Up-split
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
